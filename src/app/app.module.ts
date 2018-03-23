99ba8103a98dde519bd966b57460fd3de439481a
Ravi <ravi@ALIPL0234.local>
2018-03-23 12:41:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    ProductComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    	{
+    		path: 'product',
+    		component: ProductComponent
+    	},
+    	{
+    		path: 'member',
+    		component: MyComponentComponent
+    	}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
